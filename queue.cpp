c0e6e6ad3d3abffff9377651e4c69f1e3f454bd8
Ben Murrell <ben@benmurrell.com>
2014-07-22 23:24:41 -0500

    Implement queue and stack
    Implemented queue as a circular buffer and tested.
    
    Implemented stack as an array and tested.

--- queue.cpp
+++ queue.cpp
@@ -1,8 +1,116 @@
 // Copyright (C) 2014 Ben Murrell
 
+#include <iostream>
+#include <sstream>
+using std::cout;
+using std::endl;
+
+// FIFO Queue
+class Queue {
+    public:
+        // Constructor
+        Queue( int aDefaultCapacity = 64 ) {
+            mCapacity = aDefaultCapacity;
+            mData = new int( mCapacity );
+            mFront = 0;
+            mBack = 0;
+        }
+
+        // Destructor
+        ~Queue() {
+            // Release resources
+            delete[] mData;
+        }
+
+        // Add an item to the queue
+        void enqueue( int aItem ) {
+            // Need to expand internal storage?
+            if( mBack - mFront >= mCapacity ) {
+                // Alloc 2x the current space
+                int newCapacity = mCapacity * 2;
+                int* newStorage = new int[newCapacity];
+
+                // Copy data into new memory
+                memcpy( newStorage, mData, mCapacity * sizeof( int ) );
+
+                // Free old data
+                delete[] mData;
+
+                // Use new memory
+                mData = newStorage;
+                mCapacity = newCapacity;
+            }
+
+            mData[mBack % mCapacity] = aItem;
+            mBack++;
+        }
+
+        // Remove an item from the queue
+        int dequeue() {
+            int value = 0;
+
+            // Only dequeue if there is something in the queue
+            if( mFront < mBack ) {
+                value = mData[mFront % mCapacity];
+                mFront++;
+            }
+
+            return value;
+        }
+
+        // Get the number of items in the queue
+        int size() const {
+            return mBack - mFront;
+        }
+
+        // Get the contents in string form for testing & verification
+        std::string contents() const {
+            std::ostringstream sout;
+
+            for( int i = mFront; i < mBack; i++ ) {
+                sout << mData[i % mCapacity] << ", ";
+            }
+
+            return sout.str();
+        }
+
+    private:
+        int mCapacity;  // Current capacity of the circular buffer
+        int* mData;     // Circular buffer containing queue
+        int mFront;     // Front of circular buffer
+        int mBack;      // Back of circular buffer (location of *next* item)
+};
+
 int main( int argc, char** argv ) {
+    Queue myQ(2);
+
+    // Test adding to empty
+    myQ.enqueue( 1 );
+    cout << myQ.contents() << endl; // 1
+
+    // Test adding to non-empty
+    myQ.enqueue( 2 );
+    cout << myQ.contents() << endl; // 1 2
+
+    // Test adding beyond current capacity
+    myQ.enqueue( 3 );
+    cout << myQ.contents() << endl; // 1 2 3
+
+    myQ.enqueue( 4 );
+    cout << myQ.contents() << endl; // 1 2 3 4
+
+    // Test size
+    cout << myQ.size() << endl;     // 4
+
+    // Test removing
+    cout << myQ.dequeue() << endl;  // 1
+    cout << myQ.dequeue() << endl;  // 2
+    cout << myQ.dequeue() << endl;  // 3
+    cout << myQ.dequeue() << endl;  // 4
+    cout << myQ.size() << endl;     // 0
+
+    // Test removing from empty
+    cout << myQ.dequeue() << endl;  // 0
 
     return 0;
 }
-
-// FIFO Queue
--- stack.cpp
+++ stack.cpp
@@ -1,8 +1,129 @@
 // Copyright (C) 2014 Ben Murrell
 
+#include <iostream>
+#include <sstream>
+using std::cout;
+using std::endl;
+
+// LIFO Stack
+class Stack {
+    public:
+        // Constructor
+        Stack( int aDefaultCapacity = 64 ) {
+            mCapacity = aDefaultCapacity;
+            mData = new int[mCapacity];
+        }
+
+        // Destructor
+        ~Stack() {
+            // Release resources
+            delete[] mData;
+        }
+
+        // Push value to top of stack
+        void push( int aItem ) {
+
+            // Need to expand internal storage?
+            if( mTop >= mCapacity ) {
+                // Alloc 2x the current space
+                int newCapacity = mCapacity * 2;
+                int* newStorage = new int[newCapacity];
+
+                // Copy data into new memory
+                memcpy( newStorage, mData, mCapacity * sizeof( int ) );
+
+                // Free old data
+                delete[] mData;
+
+                // Use new memory
+                mData = newStorage;
+                mCapacity = newCapacity;
+            }
+
+            mData[mTop] = aItem;
+            mTop++;
+        }
+
+        // Pop value from top of stack
+        int pop() {
+            int value = 0;
+
+            // Only get value if stack is not empty
+            if( mTop >= 1 ) {
+                mTop--;
+                value = mData[mTop];
+            }
+
+            return value;
+        }
+
+        // Peek at value on top of stack
+        int peek() const {
+            int value = 0;
+
+            if( mTop >= 1 ) {
+                value = mData[mTop - 1];
+            }
+
+            return value;
+        }
+
+        // Get size of stack
+        int size() const {
+            return mTop;
+        }
+
+        // Get string contents for testing
+        std::string contents() const {
+            std::ostringstream sout;
+
+            for( int i = 0; i < mTop; i++ ) {
+                sout << mData[i] << ", ";
+            }
+
+            return sout.str();
+        }
+
+    private:
+        int mCapacity;  // Capacity of stack
+        int* mData;     // Container of stack
+        int mTop;       // Top of stack (location of *next* item)
+};
+
 int main( int argc, char** argv ) {
+    Stack myStack(2);
+
+    // Test adding to empty stack
+    myStack.push( 1 );
+    cout << myStack.contents() << endl; // 1
+
+    // Test adding to non-empty stack
+    myStack.push( 2 );
+    cout << myStack.contents() << endl; // 1 2
+
+    // Test adding beyond current capacity
+    myStack.push( 3 );
+    cout << myStack.contents() << endl; // 1 2 3
+
+    myStack.push( 4 );
+    cout << myStack.contents() << endl; // 1 2 3 4
+
+    // Test size
+    cout << myStack.size() << endl; // 4
+
+    // Test peek
+    cout << myStack.peek() << endl; // 4
+
+    // Test popping from stack
+    cout << myStack.pop() << endl;  // 4
+    cout << myStack.pop() << endl;  // 3
+    cout << myStack.pop() << endl;  // 2
+    cout << myStack.pop() << endl;  // 1
+    cout << myStack.size() << endl; // 0
+
+    // Test popping from empty stack
+    cout << myStack.pop() << endl;  // 0
 
     return 0;
 }
 
-// LIFO Stack
